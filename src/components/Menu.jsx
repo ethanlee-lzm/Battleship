e163b819a695354196535ff9d5c4cc0294727e33
Ethan Lee <79266746+ethanlee-lzm@users.noreply.github.com>
2021-06-08 00:47:21 -0400

    Added game starting tooltip and handling for placing ships outside boundaries

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -4,29 +4,36 @@ import Gameboard from '../components/Gameboard';
 import createGameboard from '../factories/createGameboard';
 import createShip from '../factories/createShip';
 
-import { Container, Notification, Button } from '../style';
+import MouseTooltip from 'react-sticky-mouse-tooltip';
+
+import { Container, Notification, Button, TooltipContainer } from '../style';
 
 const Menu = ({ size, startGame }) => {
   const [gameboard, setGameboard] = useState(createGameboard(size));
   const [ships, setShips] = useState([5, 4, 3, 3, 2]);
   const [isHorizontal, setIsHorizontal] = useState(true);
+  const [isFinishedPlacing, setIsFinishedPlacing] = useState(false);
   const [isInteractive, setIsInteractive] = useState(true);
+  const [isTooltipVisible, setIsTooltipVisible] = useState(false);
 
   const swapAxis = () => setIsHorizontal(!isHorizontal);
 
   const start = () => {
     setIsInteractive(false);
+    setIsFinishedPlacing(true);
     setTimeout(() => startGame(gameboard), 1000);
   };
 
   const handleClick = (x, y) => {
+    const tempGameboard = { ...gameboard };
     try {
-      gameboard.place(x, y, createShip(ships[0]), isHorizontal);
-      setGameboard(gameboard);
+      tempGameboard.place(x, y, createShip(ships[0]), isHorizontal);
+      setGameboard(tempGameboard);
       setShips(ships.slice(1));
       if (ships.length === 1) start();
     } catch (e) {
-      console.log('Ship already placed here.');
+      setIsTooltipVisible(true);
+      setTimeout(() => setIsTooltipVisible(false), 2000);
     }
   };
 
@@ -44,6 +51,14 @@ const Menu = ({ size, startGame }) => {
       <Button margin="0 0 0.5em 0" onClick={swapAxis}>
         {isHorizontal ? 'Vertical' : 'Horizontal'}
       </Button>
+      <MouseTooltip visible={isTooltipVisible} offsetX={10} offsetY={-35}>
+        <TooltipContainer width="190px">
+          Can't place ship here!
+        </TooltipContainer>
+      </MouseTooltip>
+      <MouseTooltip visible={isFinishedPlacing} offsetX={10} offsetY={-35}>
+        <TooltipContainer width="150px">Game Starting!</TooltipContainer>
+      </MouseTooltip>
     </Container>
   );
 };
